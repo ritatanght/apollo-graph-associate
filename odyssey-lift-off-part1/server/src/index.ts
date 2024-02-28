96badc4c4006aced7a7fdcf5b30c9573066f8f60
ritatanght <doggymint@gmail.com>
2024-02-28 15:12:32 -0500

    Setup the apollo server

--- odyssey-lift-off-part1/server/src/index.ts
+++ odyssey-lift-off-part1/server/src/index.ts
@@ -1,1 +1,14 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+
+const startApolloServer = async () => {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+};
+
+startApolloServer();
