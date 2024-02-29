3640df74c8cdeb234cd76eb876875b58913ea80f
ritatanght <doggymint@gmail.com>
2024-02-28 22:12:53 -0500

    Add query for getting Tracks info to tracks page

--- odyssey-lift-off-part1/client/src/pages/tracks.tsx
+++ odyssey-lift-off-part1/client/src/pages/tracks.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "../__generate__";
+
+const TRACKS = gql(`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`);
 
 /**
  * Tracks Page is the Catstronauts home page.
