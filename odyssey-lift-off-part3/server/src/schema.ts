85551cd0e0d052d41faf5d634d2ea2dc6ddde777
ritatanght <doggymint@gmail.com>
2024-03-01 13:29:41 -0500

    Add the query to get a single track by id

--- odyssey-lift-off-part3/server/src/schema.ts
+++ odyssey-lift-off-part3/server/src/schema.ts
@@ -4,6 +4,7 @@ export const typeDefs = gql`
   type Query {
     "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
