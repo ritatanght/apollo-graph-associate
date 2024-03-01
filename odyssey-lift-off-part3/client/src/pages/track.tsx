f46dfe269e53afd083323799ef40737668ba5fef
ritatanght <doggymint@gmail.com>
2024-03-01 16:43:13 -0500

    Add query to get track info and populate the page

--- odyssey-lift-off-part3/client/src/pages/track.tsx
+++ odyssey-lift-off-part3/client/src/pages/track.tsx
@@ -3,11 +3,47 @@ import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { gql } from "../__generated__";
 import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail";
 
+const GET_TRACK = gql(`
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      description
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+    }
+  }
+`);
 
 const Track = () => {
   const { trackId = "" } = useParams();
-  return <Layout></Layout>;
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: {
+      trackId,
+    },
+  });
+
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Track;
