6a0cc1afdc9838a73410749e0dfcf57db7a8b293
ritatanght <doggymint@gmail.com>
2024-03-01 17:48:19 -0500

    Add query and update UI to render the page with data

--- odyssey-lift-off-lab/client/src/pages/module.tsx
+++ odyssey-lift-off-lab/client/src/pages/module.tsx
@@ -1,9 +1,40 @@
-import { Layout, QueryResult } from "../components";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { gql } from "../__generated__";
+import { Layout, QueryResult } from "../components";
+import ModuleDetail from "../components/module-detail";
+
+const GET_MODULE_AND_PARENT_TRACK = gql(`
+query GetModule($moduleId: ID!, $trackId: ID!) {
+  module(id: $moduleId) {
+    id
+    title
+    content
+    videoUrl
+  }
+  track(id: $trackId) {
+    id
+    title
+    modules {
+      id
+      title
+      length
+    }
+  } 
+}`);
 
 const Module = () => {
   const { trackId = "", moduleId = "" } = useParams();
-  return <Layout fullWidth={true}>Module Page</Layout>;
+  const { loading, error, data } = useQuery(GET_MODULE_AND_PARENT_TRACK, {
+    variables: { trackId, moduleId },
+  });
+  return (
+    <Layout fullWidth>
+      <QueryResult error={error} loading={loading} data={data}>
+        <ModuleDetail track={data?.track} module={data?.module} />
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Module;
