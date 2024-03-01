972f631c18fc6050421060cb85d8e1c775f81415
ritatanght <doggymint@gmail.com>
2024-03-01 14:32:55 -0500

    Update trackAPI and resolvers with getting single track

--- odyssey-lift-off-part3/server/src/datasources/track-api.ts
+++ odyssey-lift-off-part3/server/src/datasources/track-api.ts
@@ -1,5 +1,5 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { TrackModel, AuthorModel } from "../models"
+import { TrackModel, AuthorModel } from "../models";
 
 export class TrackAPI extends RESTDataSource {
   baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
@@ -10,5 +10,9 @@ export class TrackAPI extends RESTDataSource {
 
   getAuthor(authorId: string) {
     return this.get<AuthorModel>(`author/${authorId}`);
+  }
+
+  getTrack(trackId: string) {
+    return this.get<TrackModel>(`track/${trackId}`);
   }
 }
