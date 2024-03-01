03fc459b234a6d34b1ee94dc4a258972dba22897
ritatanght <doggymint@gmail.com>
2024-02-29 22:33:53 -0500

    Add config for codegen with context and model

---
+++ odyssey-lift-off-part2/server/codegen.ts
@@ -0,0 +1,20 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "./src/schema.ts",
+  generates: {
+    "./src/types.ts": {
+      plugins: ["typescript", "typescript-resolvers"],
+      config: {
+        // pass the filepath to our context.ts file
+        // and point to the type we defined by #DataSourceContext
+        contextType: "./context#DataSourceContext",
+        mappers: {
+          Track: "./models#TrackModel",
+        },
+      },
+    },
+  },
+};
+
+export default config;
