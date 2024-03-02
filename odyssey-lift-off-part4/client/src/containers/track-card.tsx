ae0f48e723c7c031478477095234220c0590e018
ritatanght <doggymint@gmail.com>
2024-03-01 22:28:32 -0500

    Add the mutation to track-card and increment the views count on click

--- odyssey-lift-off-part4/client/src/containers/track-card.tsx
+++ odyssey-lift-off-part4/client/src/containers/track-card.tsx
@@ -4,6 +4,24 @@ import { colors, mq } from "../styles";
 import { humanReadableTimeFromSeconds } from "../utils/helpers";
 import { Link } from "react-router-dom";
 import type { Track } from "../__generated__/graphql";
+import { gql } from "../__generated__";
+import { useMutation } from "@apollo/client";
+
+/**
+ *  Mutation to increment a track's number of views
+ */
+const INCREMENT_TRACK_VIEWS = gql(`
+  mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+}`);
 
 /**
  * Track Card component renders basic info in a card format
@@ -12,8 +30,15 @@ import type { Track } from "../__generated__/graphql";
 const TrackCard: React.FC<{ track: Omit<Track, "modules"> }> = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
 
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    onCompleted: (data) => {
+      console.log(data);
+    },
+  });
+
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={() => incrementTrackViews()}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail || ""} alt={title} />
